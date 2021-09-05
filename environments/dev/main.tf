5924842b55c43886b3a6b2317ad605edff81b819
mike-longledge <michael.briganti@longledge.io>
2021-09-05 00:05:12 +0000

    test

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -15,11 +15,11 @@ provider "google" {
 }
 
 # Enables the Cloud Run API
-resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
-
-  disable_on_destroy = true
-}
+#resource "google_project_service" "run_api" {
+#  service = "run.googleapis.com"
+#
+#  disable_on_destroy = true
+#}
 
 # Create the Cloud Run service
 resource "google_cloud_run_service" "run_service" {
