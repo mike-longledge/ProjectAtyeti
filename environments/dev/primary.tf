48a99c10b51e699d3d768e5c4cf0e44583515672
mike-longledge <michael.briganti@longledge.io>
2021-09-05 03:52:28 +0000

    Built out first version of primary and secondary

---
+++ environments/dev/primary.tf
@@ -0,0 +1,69 @@
+# Create the Cloud Run service primary-1
+resource "google_cloud_run_service" "run_primary_1" {
+  name = "primary-1"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/google-samples/hello-app:1.0"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # Waits for the Cloud Run API to be enabled
+  depends_on = [google_project_service.run_api]
+}
+
+# Allow unauthenticated users to invoke the service
+resource "google_cloud_run_service_iam_member" "run_all_primary_1_users" {
+  service  = google_cloud_run_service.run_primary_1.name
+  location = google_cloud_run_service.run_primary_1.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+# Display the service URL
+output "primary-1-url" {
+  value = google_cloud_run_service.run_primary_1.status[0].url
+}
+
+# Create the Cloud Run service primary-2
+resource "google_cloud_run_service" "run_primary_2" {
+  name = "primary-2"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/google-samples/hello-app:1.0"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # Waits for the Cloud Run API to be enabled
+  depends_on = [google_project_service.run_api]
+}
+
+# Allow unauthenticated users to invoke the service
+resource "google_cloud_run_service_iam_member" "run_all_primary_2_users" {
+  service  = google_cloud_run_service.run_primary_2.name
+  location = google_cloud_run_service.run_primary_2.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+# Display the service URL
+output "primary-2-url" {
+  value = google_cloud_run_service.run_primary_2.status[0].url
+}
