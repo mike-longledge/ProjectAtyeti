d73530ed2ff00c3fd468e20cce0667ec02719fd5
mike-longledge <michael.briganti@longledge.io>
2021-09-05 00:59:47 +0000

    test

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -24,7 +24,7 @@ resource "google_project_service" "run_api" {
 # Create the Cloud Run service
 resource "google_cloud_run_service" "run_service" {
   name = "app"
-  location = "us-central1"
+  location = "us-east1"
 
   template {
     spec {
