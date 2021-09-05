be5235d5953cf60f4ee79a7736d5e9d18e3c3b18
mike-longledge <michael.briganti@longledge.io>
2021-09-05 19:39:50 +0000

    Added load balancing scripts

---
+++ scripts/loadBalancer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+declare -A endpoint
+
+endpoint[primary-1-url]="https://primary-1-juucpiowaa-ue.a.run.app"
+endpoint[primary-2-url]="https://primary-2-juucpiowaa-ue.a.run.app"
+endpoint[secondary-1-url]="https://secondary-1-jecs2yle3a-uc.a.run.app"
+endpoint[secondary-2-url]="https://secondary-2-jecs2yle3a-uc.a.run.app"
+
+function check_health () {
+   curl -f ${endpoint[${1}-1-url]} &>/dev/null  && \
+     curl -f ${endpoint[${1}-2-url]} &>/dev/null 
+   echo $?
+}
+
+function round_robin() {
+   curl -f ${endpoint[${1}-1-url]}
+   curl -f ${endpoint[${1}-2-url]}
+}
+
+while true; do
+ if [[ $(check_health primary) == '0' ]]; then
+  round_robin primary
+ elif [[ $(check_health secondary) == '0' ]]; then
+  round_robin secondary
+ else 
+  echo "Atyeti: Both Region Down"
+ fi
+ sleep 5
+done
